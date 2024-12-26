a52faf561758220436f06ee7ba020c3afd15541d
Tommy Ngo <tommyhoaingo@gmail.com>
2024-12-26 15:09:21 -0600

    darwin & nixos - sync keybinds for WM

--- modules/darwin/system/aerospace.nix
+++ modules/darwin/system/aerospace.nix
@@ -10,37 +10,44 @@
         outer.top = 2;
         outer.right = 4;
       };
-      # TODO: Add resizing bindings - update linux bindings to somehwat match
       mode.main.binding = {
         # See: https://nikitabobko.github.io/AeroSpace/commands#layout
         alt-slash = "layout tiles horizontal vertical";
 
+        # Moving windows within layout - Vim
         alt-h = "focus left";
         alt-j = "focus down";
         alt-k = "focus up";
         alt-l = "focus right";
+        alt-shift-h = "move left";
+        alt-shift-j = "move down";
+        alt-shift-k = "move up";
+        alt-shift-l = "move right";
 
+        # Switching Workspaces and moving to Workspaces
         alt-1 = "workspace 1";
         alt-2 = "workspace 2";
         alt-3 = "workspace 3";
         alt-4 = "workspace 4";
-        alt-5 = "workspace 5";
-        alt-6 = "workspace 6";
-        alt-7 = "workspace 7";
-        alt-8 = "workspace 8";
+        # alt-5 = "workspace 5";
+        # alt-6 = "workspace 6";
+        # alt-7 = "workspace 7";
+        # alt-8 = "workspace 8";
         alt-a = "workspace prev --wrap-around";
         alt-d = "workspace next --wrap-around";
-
         alt-shift-1 = "move-node-to-workspace 1";
         alt-shift-2 = "move-node-to-workspace 2";
         alt-shift-3 = "move-node-to-workspace 3";
         alt-shift-4 = "move-node-to-workspace 4";
-        alt-shift-5 = "move-node-to-workspace 5";
-        alt-shift-6 = "move-node-to-workspace 6";
-        alt-shift-7 = "move-node-to-workspace 7";
-        alt-shift-8 = "move-node-to-workspace 8";
+        # alt-shift-5 = "move-node-to-workspace 5";
+        # alt-shift-6 = "move-node-to-workspace 6";
+        # alt-shift-7 = "move-node-to-workspace 7";
+        # alt-shift-8 = "move-node-to-workspace 8";
         alt-shift-a = "move-node-to-workspace prev --wrap-around";
         alt-shift-d = "move-node-to-workspace next --wrap-around";
+
+        alt-ctrl-left = "resize smart -50";
+        alt-ctrl-right = "resize smart +50";
       };
     };
   };
