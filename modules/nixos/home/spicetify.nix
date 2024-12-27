514ca7f9025c090f6ce531b28104ad8b6331c529
Tommy Ngo <tommyhoaingo@gmail.com>
2024-12-27 00:19:10 -0600

    nixos

--- modules/nixos/home/spicetify.nix
+++ modules/nixos/home/spicetify.nix
@@ -1,14 +1,14 @@
 {
-  spicetify-nix,
+  inputs,
   pkgs,
   ...
 }: {
   imports = [
-    spicetify-nix.homeManagerModules.default
+    inputs.spicetify-nix.homeManagerModules.default
   ];
 
   programs.spicetify = let
-    spicePkgs = spicetify-nix.legacyPackages.${pkgs.system};
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
   in {
     enable = true;
     enabledExtensions = with spicePkgs.extensions; [
