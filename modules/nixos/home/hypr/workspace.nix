9630034561bb97923e2ac0c41b92e48c4e0d7e2d
Tommy Ngo <tommyhoaingo@gmail.com>
2024-12-28 16:44:12 -0600

    point to local install of ags

--- modules/nixos/home/hypr/workspace.nix
+++ modules/nixos/home/hypr/workspace.nix
@@ -1,11 +1,11 @@
 {
   wayland.windowManager.hyprland.settings = {
     workspace = [
-      "1, , persistent:true, default:true"
-      "2, , persistent:true"
-      "3, , persistent:true"
-      "4, , persistent:true"
-      "5, , persistent:true"
+      "1, persistent:true, default:true"
+      "2, persistent:true"
+      "3, persistent:true"
+      "4, persistent:true"
+      "5, persistent:true"
     ];
   };
 }
